337b4aa2c7e27ad58386446caa812c337afdadbb
Renan <orefusado@gmail.com>
2022-12-09 02:29:10 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardCoverComponent } from './components/card-cover/card-cover.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleComponent,
+    HeaderComponent,
+    NavBarComponent,
+    CardCoverComponent,
+    MainCardComponent,
+    SmallCardComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-cover/card-cover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-cover',
+  templateUrl: './card-cover.component.html',
+  styleUrls: ['./card-cover.component.css']
+})
+export class CardCoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
