942dca2892e76021974614c6188ec177e8b8fbb4
Renan <orefusado@gmail.com>
2022-12-19 05:16:05 -0300

    feat: navbar resopnsivo

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css', './nav-bar.component.desktop.css']
 })
 export class NavBarComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  navbarActive: boolean = true;
+  toggleNavbar(): void {
+    this.navbarActive = !this.navbarActive;
   }
 
 }
