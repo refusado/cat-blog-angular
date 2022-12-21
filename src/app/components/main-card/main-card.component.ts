6ff47761f7248fb1b76bdae954756243af95bd89
Renan <orefusado@gmail.com>
2022-12-21 07:14:15 -0300

    feat: melhorias responsividade

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
-  styleUrls: ['./main-card.component.css']
+  styleUrls: ['./main-card.component.css', './main-card.component.css']
 })
 export class MainCardComponent implements OnInit {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { fakeData } from 'src/app/data/fakeData';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', './home.component.desktop.css']
 })
 export class HomeComponent implements OnInit {
   mainArticle: Article = {
