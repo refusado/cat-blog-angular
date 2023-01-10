d8cff567e5e8ef25f5961f1604271cbde428da6e
Renan <orefusado@gmail.com>
2023-01-10 04:32:54 -0300

    feat: mais estilos responsivos

--- src/app/pages/home-page/contact-form/contact-form.component.ts
+++ src/app/pages/home-page/contact-form/contact-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css', './contact-form.component.desktop.css']
 })
 export class ContactFormComponent implements OnInit {
 
--- src/app/pages/home-page/small-card/small-card.component.ts
+++ src/app/pages/home-page/small-card/small-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', './small-card.component.desktop.css']
 })
 export class SmallCardComponent implements OnInit {
 
