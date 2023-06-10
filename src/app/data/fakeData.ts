3673c163d3de76141f5dc486854a97d5b681885e
Renan <orefusado@gmail.com>
2023-06-10 15:30:06 -0300

    change contents + deploy

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -2,19 +2,91 @@ export const fakeData = [
   {
     "id": "0",
     "image": "./assets/blog-img.jpg",
-    "title": "Os maiores gatos da história do Brasil",
-    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus quasi, sapiente atque quas et mollitia, nam at tempore, dolorem facere. Consequatur eligendi possimus eius facere maiores atque, culpa quasi? Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "title": "A Existência dos Gatos de Outro Mundo: Mito ou Realidade?",
+    "content": `<h1>Artigo: Os gatos realmente gostam dos humanos?</h1>
+    <h2 id="introdu-o">Introdução</h2>
+    <p>A relação entre gatos e humanos tem sido objeto de curiosidade e debate ao longo dos anos. Enquanto os cães são conhecidos por sua lealdade inquestionável e amor incondicional, os gatos muitas vezes são considerados animais independentes e reservados. No entanto, os amantes de gatos argumentam que esses felinos também são capazes de demonstrar afeto e carinho genuínos. Neste artigo, vamos explorar mais a fundo essa questão intrigante: os gatos realmente gostam dos humanos?</p>
+    <h2 id="comportamentos-afetuosos-dos-gatos">Comportamentos afetuosos dos gatos</h2>
+    <p>Embora os gatos possam não ser tão efusivos quanto os cães em expressar seu amor, eles possuem uma linguagem própria para mostrar afeto. Comportamentos como ronronar, esfregar-se nas pernas dos humanos, lamber ou dar pequenas mordidinhas suaves são sinais de afeição felina. Esses gestos demonstram que os gatos se sentem seguros, confortáveis e confiantes na presença de seus donos.</p>
+    <h2 id="v-nculo-com-os-humanos">Vínculo com os humanos</h2>
+    <p>Embora os gatos sejam animais independentes, eles têm a capacidade de formar laços emocionais com seus donos. Estudos científicos têm mostrado que gatos domésticos podem desenvolver um forte senso de segurança e confiança em relação aos seus cuidadores humanos. Esses laços afetivos são construídos ao longo do tempo, à medida que o gato percebe que o ambiente é seguro, suas necessidades são atendidas e há interações positivas com os humanos.</p>
+    <h2 id="procura-por-intera-o">Procura por interação</h2>
+    <p>Embora os gatos valorizem sua independência, muitos deles procuram ativamente a companhia de seus donos. Eles podem seguir seus donos pela casa, sentar-se próximo a eles ou até mesmo dormir perto deles. Essas ações mostram que os gatos gostam da presença e da proximidade humana. Além disso, os gatos podem se envolver em brincadeiras e interações com os humanos, buscando atenção e estimulação social.</p>
+    <h2 id="comunica-o-n-o-verbal">Comunicação não verbal</h2>
+    <p>Os gatos têm uma forma única de se comunicar com os humanos. Eles usam sinais não verbais, como expressões faciais, posturas corporais e movimentos da cauda e das orelhas, para transmitir suas emoções. Por exemplo, o piscar lento dos olhos de um gato pode ser interpretado como um sinal de confiança e afeto. Ao aprender a interpretar esses sinais, os donos podem entender melhor o que seus gatos estão sentindo e responder adequadamente, fortalecendo ainda mais o vínculo entre eles.</p>
+    <h2 id="benef-cios-m-tuos">Benefícios mútuos</h2>
+    <p>A relação entre gatos e humanos é benéfica para ambas as partes. Os gatos proporcionam companhia e podem trazer alívio do estresse e conforto emocional para seus donos. Eles podem até mesmo desempenhar um papel terapêutico em casos de ansiedade ou depressão. Por sua vez, os humanos fornecem abrigo, alimentação e cuidados veterinários, além de afeto e atenção aos gatos. Essa troca mútua</p>
+    <p> de cuidado e amor é o que sustenta a relação entre humanos e gatos.</p>
+    <h2 id="varia-es-individuais">Variações individuais</h2>
+    <p>É importante lembrar que cada gato é único e pode ter diferentes temperamentos e preferências. Alguns gatos são naturalmente mais afetuosos e demonstrativos, enquanto outros podem ser mais reservados. Além disso, a experiência prévia do gato, seu histórico de socialização e sua personalidade desempenham um papel importante em como eles interagem com os humanos. É essencial respeitar as individualidades de cada gato e permitir que eles estabeleçam seu próprio nível de proximidade e interação.</p>
+    <h2 id="conclus-o">Conclusão</h2>
+    <p>Embora a reputação de independência dos gatos possa sugerir o contrário, muitos deles realmente gostam dos humanos e são capazes de formar laços afetivos profundos. Seus comportamentos afetuosos, a procura por interação e a comunicação não verbal são evidências de seu apreço pelos humanos. No entanto, é fundamental lembrar que a relação entre gatos e humanos varia de um indivíduo para outro. Cada gato tem sua própria personalidade e preferências, e é importante respeitar seus limites e permitir que eles estabeleçam seu próprio ritmo de interação. Com paciência, compreensão e amor, você pode cultivar uma relação duradoura e gratificante com seu gato, desfrutando de todo o afeto e companheirismo que eles têm a oferecer.</p>
+    `,
   },
   {
     "id": "1",
     "image": "./assets/blog-img2.jpg",
-    "title": "Os gatos realmente não gostam dos humanos?",
-    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus quasi, sapiente atque quas et mollitia, nam at tempore, dolorem facere. Consequatur eligendi possimus eius facere maiores atque, culpa quasi?. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus quasi, sapiente atque quas et mollitia, nam at tempore, dolorem facere. Consequatur eligendi possimus eius facere maiores atque, culpa quasi?",
+    "title": "Os gatos realmente gostam dos humanos?",
+    "content": `<h1>A Existência dos Gatos de Outro Mundo: Mito ou Realidade?</h1>
+    <p><img src="https://example.com/imagem-gatos-outro-mundo.jpg" alt="Gatos de Outro Mundo"></p>
+    <h2 id="introdu-o">Introdução</h2>
+    <p>Os gatos sempre exerceram fascínio sobre os seres humanos. Sua natureza misteriosa e comportamento peculiar levantam questionamentos e teorias intrigantes. Um dos temas mais controversos e cativantes é a possibilidade de existirem gatos de outro mundo, criaturas vindas de dimensões desconhecidas. Neste artigo, mergulharemos no universo dos gatos de outro mundo, explorando relatos, lendas e possíveis explicações científicas para esse enigma.</p>
+    <h2 id="os-gatos-na-mitologia-e-nas-lendas-antigas">Os gatos na mitologia e nas lendas antigas</h2>
+    <p>Desde os tempos antigos, os gatos têm desempenhado um papel significativo nas mitologias e lendas de várias culturas ao redor do mundo. Em muitas dessas histórias, os gatos são retratados como seres sobrenaturais, associados a poderes místicos e divindades. Por exemplo, no Egito Antigo, os gatos eram considerados sagrados e adorados como encarnações de deuses. Na mitologia nórdica, o gato gigante chamado Jormungand era uma das criaturas míticas mais temidas.</p>
+    <h2 id="relatos-de-encontros-com-gatos-de-outro-mundo">Relatos de encontros com gatos de outro mundo</h2>
+    <p>Ao longo dos séculos, inúmeros relatos de encontros com gatos misteriosos e incomuns surgiram em diferentes partes do mundo. Esses relatos descrevem gatos com características fora do comum, como olhos hipnotizantes, habilidades sobrenaturais, ou mesmo a capacidade de desaparecer e reaparecer em lugares distintos.</p>
+    <p>Um exemplo famoso é o caso do &quot;Gato do Espaço&quot;, ocorrido em 1957. Nesse incidente, um gato preto apareceu em uma espaçonave soviética que retornava de uma missão. Astronautas afirmaram que o felino estava completamente fora do lugar, sugerindo uma possível origem extraterrestre.</p>
+    <h2 id="explica-es-cient-ficas-para-gatos-de-outro-mundo">Explicações científicas para gatos de outro mundo</h2>
+    <p>Embora a existência de gatos de outro mundo pareça fantástica, alguns pesquisadores e cientistas propuseram teorias para explicar esses fenômenos. Uma dessas teorias é a possibilidade de que os gatos tenham habilidades telepáticas, permitindo-lhes acessar dimensões paralelas ou comunicar-se com seres de outras realidades.</p>
+    <p>Outra explicação envolve o campo da física quântica, que postula a existência de múltiplos universos e realidades alternativas. Segundo essa teoria, os gatos poderiam ser criaturas capazes de atravessar as barreiras entre os universos, aparecendo e desaparecendo em nossa realidade de forma inexplicável.</p>
+    <h2 id="os-gatos-como-guardi-es-dos-portais-dimensionais">Os gatos como guardiões dos portais dimensionais</h2>
+    <p>Uma crença interessante é a de que os gatos desempenham o papel de guardiões dos portais dimensionais, sendo capazes de viajar entre diferentes planos de existência. Essa teoria sugere que os gatos possuem uma conexão especial com o desconhecido e podem proteger a humanidade de forças ocultas ou influências negativas</p>
+    <p> vindas de outras dimensões.</p>
+    <h2 id="o-mist-rio-continua">O mistério continua</h2>
+    <p>Apesar dos relatos e teorias fascinantes, a existência dos gatos de outro mundo ainda é um mistério não solucionado. A falta de evidências científicas concretas e a natureza esquiva desses felinos mantêm o tema no campo do desconhecido e do sobrenatural. Talvez nunca tenhamos respostas definitivas para essas perguntas intrigantes, mas a aura de mistério que envolve os gatos continua a nos encantar e alimentar nossa imaginação.</p>
+    <h2 id="conclus-o">Conclusão</h2>
+    <p>Os gatos de outro mundo representam um enigma cativante que tem sido objeto de fascínio e especulação ao longo dos tempos. Embora não haja evidências científicas conclusivas para provar sua existência, os relatos históricos, as lendas e as teorias sugerem que esses felinos podem possuir habilidades e origens que ultrapassam nosso entendimento convencional. Independentemente de sua origem real ou imaginada, os gatos de outro mundo continuam a despertar nossa curiosidade e manter vivo o mistério que envolve esses seres adoráveis e enigmáticos.</p>
+    `,
   },
   {
     "id": "2",
     "image": "./assets/blog-img3.jpg",
-    "title": " 274 motivos para você adotar um gato hoje mesmo",
-    "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus quasi, sapiente atque quas et mollitia, nam at tempore, dolorem facere. Consequatur eligendi possimus eius facere maiores atque, culpa quasi?",
+    "title": "14 Motivos para Você Adotar um Gato",
+    "content": `<h1>14 Motivos para Você Adotar um Gato</h1>
+    <p><img src="https://example.com/gatinho-fofo.jpg" alt="Gatinho fofo"></p>
+    <h2 id="introdu-o">Introdução</h2>
+    <p>Adotar um gato é uma decisão que traz muitas alegrias e benefícios para a vida de uma pessoa. Esses animais adoráveis e independentes têm conquistado o coração de milhões de pessoas em todo o mundo. Neste artigo, apresentaremos 14 motivos convincentes para você considerar a adoção de um gato. Prepare-se para se encantar e se convencer de que um gato pode ser o companheiro perfeito para você!</p>
+    <h2 id="1-companheirismo-sem-igual">1. Companheirismo sem igual</h2>
+    <p>Os gatos são excelentes companheiros. Eles podem preencher sua vida com amor, carinho e momentos de diversão. A presença de um gato em casa traz uma sensação de conforto e acolhimento.</p>
+    <h2 id="2-alegria-e-divers-o-garantidas">2. Alegria e diversão garantidas</h2>
+    <p>Os gatos são criaturas brincalhonas e curiosas. Eles adoram perseguir brinquedos, explorar o ambiente e se divertir com jogos interativos. Ter um gato em casa certamente trará mais alegria e diversão para sua rotina.</p>
+    <h2 id="3-redu-o-do-estresse-e-ansiedade">3. Redução do estresse e ansiedade</h2>
+    <p>A simples presença de um gato pode ajudar a reduzir o estresse e a ansiedade. Acariciar um gato ou observá-lo brincando pode ter um efeito calmante e relaxante, proporcionando um alívio para o dia a dia agitado.</p>
+    <h2 id="4-companhia-para-pessoas-solit-rias">4. Companhia para pessoas solitárias</h2>
+    <p>Se você vive sozinho ou se sente solitário em alguns momentos, um gato pode ser a solução ideal. Esses felinos são excelentes companheiros para pessoas que buscam amor e carinho, mas que também apreciam a independência e a personalidade única dos gatos.</p>
+    <h2 id="5-terapia-emocional">5. Terapia emocional</h2>
+    <p>A presença de um gato pode oferecer suporte emocional e até mesmo ser considerada uma terapia. A interação com esses animais pode ajudar a combater a depressão, melhorar o humor e aumentar a sensação de bem-estar.</p>
+    <h2 id="6-responsabilidade-e-senso-de-prop-sito">6. Responsabilidade e senso de propósito</h2>
+    <p>Adotar um gato implica em assumir a responsabilidade de cuidar de outro ser vivo. Essa responsabilidade traz consigo um senso de propósito, pois você se torna responsável pelo bem-estar e felicidade do seu gato. Cuidar de um gato ensina lições valiosas sobre responsabilidade e empatia.</p>
+    <h2 id="7-conex-o-com-a-natureza">7. Conexão com a natureza</h2>
+    <p>Os gatos têm uma conexão especial com a natureza. Observar seu comportamento ágil e instintivo pode ajudar a reconectar-se com o mundo natural. A presença de um gato em sua casa traz um lembrete constante da beleza e da harmonia do reino animal.</p>
+    <h2 id="8-melhoria-da-sa-de-cardiovascular">8. Melhoria da saúde cardiovascular</h2>
+    <p>Estudos científicos sugerem que ter um gato pode melhorar a saúde cardiovascular. A presença de um gato em casa está associada a níveis mais baixos de estresse, pressão arterial mais saudável e menor risco de doenças cardiovasculares.</p>
+    <h2 id="9-companhia-para-crian-as">9. Companhia para crianças</h2>
+    <p>Ter um gato</p>
+    <p> em casa pode ser extremamente benéfico para as crianças. Além de ensiná-las a cuidar de outro ser vivo, os gatos podem se tornar amigos leais e confidentes, oferecendo um vínculo emocional duradouro.</p>
+    <h2 id="10-est-mulo-mental">10. Estímulo mental</h2>
+    <p>Os gatos são animais inteligentes e curiosos. Proporcionar um ambiente enriquecedor para seu gato, com brinquedos interativos e desafios mentais, pode estimular sua mente e mantê-lo mentalmente ágil e ativo.</p>
+    <h2 id="11-compatibilidade-com-diferentes-estilos-de-vida">11. Compatibilidade com diferentes estilos de vida</h2>
+    <p>Os gatos são animais versáteis e se adaptam a diferentes estilos de vida. Seja você uma pessoa ocupada, um estudante ou um aposentado, um gato pode se ajustar ao seu ritmo de vida e trazer alegria em todas as fases.</p>
+    <h2 id="12-cuidados-simples">12. Cuidados simples</h2>
+    <p>Os gatos são animais relativamente independentes e seus cuidados são simples em comparação com outros animais de estimação. Eles são higiênicos por natureza e não precisam de banhos frequentes. A manutenção de sua higiene, alimentação adequada e consultas regulares ao veterinário são suficientes para garantir seu bem-estar.</p>
+    <h2 id="13-aprendizado-constante">13. Aprendizado constante</h2>
+    <p>Conviver com um gato é uma experiência de aprendizado contínuo. Eles têm comportamentos únicos e interessantes, e observar e entender seu mundo felino pode ser uma fonte inesgotável de conhecimento e descobertas.</p>
+    <h2 id="14-amor-incondicional">14. Amor incondicional</h2>
+    <p>Por último, mas certamente não menos importante, os gatos oferecem amor incondicional aos seus donos. Eles têm uma capacidade única de criar laços profundos e verdadeiros com seus cuidadores. O amor de um gato é genuíno, puro e duradouro.</p>
+    <h2 id="conclus-o">Conclusão</h2>
+    <p>Adotar um gato pode ser uma das melhores decisões que você pode tomar. Além de trazer alegria, diversão e companhia, esses animais adoráveis oferecem uma série de benefícios físicos, emocionais e mentais. A adoção de um gato é um gesto nobre, pois você está oferecendo uma segunda chance a um animal que precisa de um lar amoroso. Então, não espere mais! Considere a adoção de um gato e permita que esses seres encantadores façam parte da sua vida e tragam felicidade e amor duradouro.</p>
+    `,
   }
 ]
